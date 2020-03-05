28d31b6d05ed3b8e5bdbd27562e51f64e22045d0
jomno <jake8955@naver.com>
2020-03-06 00:31:58 +0900

    go master

---
+++ app/admin/users.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :bot_user_key, :plusfriend_user_key
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:bot_user_key, :plusfriend_user_key]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
