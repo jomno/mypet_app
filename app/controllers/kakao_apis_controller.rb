8b13d5533f07c75de4756a3690847815958fc61e
jomno <jake8955@naver.com>
2020-03-05 17:06:22 +0900

    버그 수정

--- app/controllers/kakao_apis_controller.rb
+++ app/controllers/kakao_apis_controller.rb
@@ -2,10 +2,11 @@ class KakaoApisController < ApplicationController
   def create
     params = request.request_parameters
     saved_data = KakaoApi.create(input_data: params)
+    byebug
     render json: {
         "version": "2.0",
         "data": {
-          "msg": params.dig("action", "params, "species")
+          "msg": params.dig("action", "params", "species")
         }
       }
   end
