8e6847f6cc276ce18c6938a359f1901653823006
jomno <jake8955@naver.com>
2020-03-06 01:02:41 +0900

    응답 대기 중

--- app/controllers/kakao_apis_controller.rb
+++ app/controllers/kakao_apis_controller.rb
@@ -3,13 +3,20 @@ class KakaoApisController < ApplicationController
     params = request.request_parameters
     bot_user_key = params.dig("userRequest", "user", "properties", "bot_user_key")
     plusfriend_user_key = params.dig("userRequest", "user", "properties", "plusfriend_user_key")
-    user = User.find_or_create_by(bot_user_key: bot_user_key, plusfriend_user_key: plusfriend_user_key)
+    user = User.find_or_create_by(bot_user_key: params[:bot_user_key], plusfriend_user_key: params[:plusfriend_user_key])
+    # user = User.find_or_create_by(bot_user_key: bot_user_key, plusfriend_user_key: plusfriend_user_key)
     saved_data = KakaoApi.create(input_data: params, user: user)
     render json: {
-      "version": "2.0",
-      "data": {
-        "msg": "다시 한 번 말씀해주세요!" # params.dig("action", "params", "species")
-      }
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "다시 말씀해주세요!"
+                    }
+                }
+            ]
+        }
     }
   end
 
@@ -17,15 +24,22 @@ class KakaoApisController < ApplicationController
     params = request.request_parameters
     bot_user_key = params.dig("userRequest", "user", "properties", "bot_user_key")
     plusfriend_user_key = params.dig("userRequest", "user", "properties", "plusfriend_user_key")
-    user = User.find_or_create_by(bot_user_key: bot_user_key, plusfriend_user_key: plusfriend_user_key)
+    user = User.find_or_create_by(bot_user_key: params[:bot_user_key], plusfriend_user_key: params[:plusfriend_user_key])
+    # user = User.find_or_create_by(bot_user_key: bot_user_key, plusfriend_user_key: plusfriend_user_key)
     saved_data = KakaoApi.create(input_data: params, user: user)
 
 
     render json: {
-      "version": "2.0",
-      "data": {
-        "msg": "응답 대기 중" # params.dig("action", "params", "species")
-      }
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "간단한 텍스트 요소입니다."
+                    }
+                }
+            ]
+        }
     }
   end
   
