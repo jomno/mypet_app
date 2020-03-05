fcba1838a14cf3d87109d725fd283f52c98dea86
jomno <jake8955@naver.com>
2020-03-05 15:42:23 +0900

    data 저장

--- app/controllers/kakao_apis_controller.rb
+++ app/controllers/kakao_apis_controller.rb
@@ -1,11 +1,10 @@
 class KakaoApisController < ApplicationController
   def create
-    param = params.permit(:userRequest, :bot, :action)
+    params = request.request_parameters
     logger.info "params: #{params}"
     logger.info "userRequest: #{params[:userRequest]}"
     logger.info "bot: #{params[:bot]}"
     logger.info "action: #{params[:action]}"
-    logger.info "param: #{param}"
     render json: {response: :success}    
   end
 end
