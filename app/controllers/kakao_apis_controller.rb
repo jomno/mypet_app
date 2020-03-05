7a26ffcfe665bdd1446fc3458c6917d4772bd3c4
jomno <jake8955@naver.com>
2020-03-05 17:03:04 +0900

    json 수정

--- app/controllers/kakao_apis_controller.rb
+++ app/controllers/kakao_apis_controller.rb
@@ -1,17 +1,11 @@
 class KakaoApisController < ApplicationController
   def create
     params = request.request_parameters
-    logger.info "params: #{params}"
-    logger.info "userRequest: #{params[:userRequest]}"
-    logger.info "bot: #{params[:bot]}"
-    logger.info "action: #{params[:action]}"
     saved_data = KakaoApi.create(input_data: params)
     render json: {
         "version": "2.0",
         "data": {
-          "msg":"HI",
-          "name":"Ryan",
-          "position":"Senior Managing Director"
+          "msg": params.dig("action", "params, "species")
         }
       }
   end
