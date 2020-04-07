201d530e167a3408e9aefa0a2103537e4c7fe8d7
wonno <jake89577@gmail.com>
2020-04-07 12:04:31 +0900

    에러 제거

--- app/controllers/api/v1/bots_controller.rb
+++ app/controllers/api/v1/bots_controller.rb
@@ -9,30 +9,6 @@ module Api
                 species_name = @params.dig("action", "params", "species") if Rails.env.production?
 
                 species = Species.find_by(name: species_name)
-
-                case intent
-                when "개 증상 확인하기"
-                    result = base_template
-                    card_ary = []
-                    present_card = {}
-
-                    species.symptoms.each do |symptom|
-                        if present_card.empty?
-                            present_card = basic_card_template("#{species.name}가 아프시군요.", "증상을 선택해주세요.")
-                        elsif present_card.dig(:basicCard, :buttons).length < 3
-                            present_card.dig(:basicCard, :buttons) << buttons_template(symptom.name, "#{species.name}종이 [#{symptom.name}] 증상이 있습니다.")
-                        elsif present_card.dig(:basicCard, :buttons).length == 3
-                            card_ary << present_card
-                            present_card = {}
-                        end
-                    end
-                    basic_card_template("#{species.name}가 아프시군요.")
-                    result.dig(:template, :outputs) << 
-                when "고양이 증상 확인하기"
-                    result = default_json
-                end
-                
-                render json: result
             end
         end
     end
