99d70f794c4fdda6e37dbc6d2f4d5bf7056c34e2
wonno <jake89577@gmail.com>
2020-04-12 17:58:11 +0900

    izitoast 추가

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,3 +1,28 @@
 //= require active_admin/base
 //= require active_admin/sortable
 //= require tinymce-jquery
+//= require izitoast/dist/js/iziToast.js
+
+
+$(function(){
+  var elements = document.getElementsByClassName("col-link");
+  
+  var copyLink = function (){
+    const el = document.createElement('textarea');
+    str = this.innerText;
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    iziToast.success({
+      title: '복사 성공!',
+      position: 'center',
+      message: str,
+    });
+  };
+  
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', copyLink, false);
+  }
+})
