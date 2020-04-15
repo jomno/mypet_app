00d02f8a10b71e141a54b51f31ad8904d8bcdf04
wonno <jake89577@gmail.com>
2020-04-15 14:59:15 +0900

    필요없는 기능 제거

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,10 +2,4 @@ class SolutionsController < ApplicationController
   def show
     @solution = Solution.find_by(s_id: params[:id])
   end
-
-  def link
-    solution = Solution.find params[:id]
-
-    redirect_to solution.link
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'solutions/show'
-  get 'maps/show'
-  resources :solutions, only: [:show] do 
-    member do
-      get :link
-    end
-  end
-  post 'kakao_apis/get_place'
-  post 'kakao_apis/create'
-  post 'kakao_apis/get_solution'
-  post 'kakao_apis/reset'
+  resources :solutions, only: [:show]
+  # post 'kakao_apis/get_place'
+  # post 'kakao_apis/create'
+  # post 'kakao_apis/get_solution'
+  # post 'kakao_apis/reset'
   root "admin/dashboard#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
